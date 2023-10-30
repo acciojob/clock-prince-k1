d9f9b401a5f37928c622058ed9e140bcb2df3e74
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-10-30 21:10:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+
+
+
+let p = document.getElementById('timer');
+
+
+function updateTime(){
+let month = new Date().getMonth();
+let date = new Date().getDate();
+let year = new Date().getFullYear();
+let hour = new Date().getHours();
+let min = new Date().getMinutes();
+let sec = new Date().getSeconds();
+let ans = `${month}/${date}/${year}, ${hour}:${min}:${sec}`;
+p.innerText = ans;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
